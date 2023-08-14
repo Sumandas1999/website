81822c1d9d8528763a1a956b6f8a9e45abb94b35
suman <ku2sumandas1999@gmail.com>
2023-08-14 11:02:55 +0530

    fix: minor updates

--- src/component/sideBar.js
+++ src/component/sideBar.js
@@ -7,11 +7,11 @@ import mail from '../ICONS/gmail.svg'
 
 function Side() {
   return (
-    <div className= ' social-bar flex flex-col  h-auto  w-16 space-y-8 justify-center items-center pt-2 rounded-lg sticky top-12 '>
-        <a href='https://www.instagram.com/__s____d_/'> <img className='social h-5vh  border-solid border-2 border-sky-500 rounded-lg  ' src={insta} ></img> </a>
-       <a href='https://twitter.com/Sumandas614319'><img className='social h-5vh  border-solid border-2 border-sky-500 rounded-lg' src={twitter}></img> </a> 
-        <a href='ku2sumandas19999@gmail.com'><img className='social h-5vh  border-solid border-2 border-sky-500 rounded-lg' src={mail}></img> </a>
-        <img className='social h-5vh  border-solid border-2 border-sky-500 rounded-lg hiddden' src={meta}></img>
+    <div className= ' social-bar flex flex-col  h-auto  w-16 space-y-8 justify-center items-center pt-2  sticky top-12 '>
+        <a href='https://www.instagram.com/__s____d_/'> <img className='social h-5vh  ' src={insta} ></img> </a>
+       <a href='https://twitter.com/Sumandas614319'><img className='social h-5vh  ' src={twitter}></img> </a> 
+        <a href='ku2sumandas19999@gmail.com'><img className='social h-5vh  ' src={mail}></img> </a>
+        <img className='social h-5vh   hiddden' src={meta}></img>
     </div>
   )
 }
