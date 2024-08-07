17247e1c5f59ad0597493056a8a5b90498b89338
suman <ku2sumandas1999@gmail.com>
2024-08-07 21:51:49 +0530

    lazy load

--- src/component/footer.js
+++ src/component/footer.js
@@ -9,14 +9,7 @@ function Footer() {
         <h2 className='footer-text text-white text-3xl'>"For me, the camera is a sketchbook, an instrument of intuition and spontaneity."</h2>
 
       </div>
-      <div className=' flex h-24 text-5xl justify-between mr-10'>
-        <h1>
-          i am done with coding
-        </h1>
-        
-
-      </div>
-
+     
     </div>
 
 
